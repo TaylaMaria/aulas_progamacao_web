b0470f5bc2f4d2e21faea2acbdaee2d1ffcb7401
TaylaMaria <taylamariasousagomes@gmail.com>
2025-10-10 14:08:23 -0300

    primeiro commit

---
+++ teste.py
@@ -0,0 +1,4 @@
+#pip install requeriments.txt comando para instalar versoes no notebook do cliente
+#.\venv\Scripts\activate.ps1 para ativar o ambiente virtual toda vez que iniciar o vscode
+#django-admin help para ter acesso a todos os comandos do django
+#para abrir o link: python manage.py runserver
